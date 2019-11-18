ad07b8121f178f55ba8a3cc5a8ab4d4389e61a2a
Jophin Joseph <jophin.joseph88@gmail.com>
2019-11-18 13:33:04 +0530

    Adding test case for nested combine reducers

--- tests/combinereducers.test.js
+++ tests/combinereducers.test.js
@@ -1,4 +1,5 @@
 import combineReducers from "../src/combinereducers";
+import { isUndefined } from "@kubric/litedash";
 
 const actions = {
   ADD: 'ADD',
@@ -90,6 +91,44 @@ describe("combineReducer tests", () => {
       two: 2
     }, 3);
     expect(finalState === initialState).toEqual(true);
+  });
+
+  it("nested combine reducers", () => {
+    const reducer1 = (state, action, extState) => isUndefined(extState) ? 0 : extState;
+    const reducer3 = (state, action, subExtState, extState) => {
+      if (!isUndefined(subExtState) && !isUndefined(extState)) {
+        return {
+          extState,
+          subExtState
+        };
+      } else {
+        return 0;
+      }
+    };
+    const nestedCombinedReducer = combineReducers({
+      reducer3
+    });
+    const reducer2 = (state, action, extState) => nestedCombinedReducer(state, action, state, extState);
+
+    const combinedReducer = combineReducers({
+      reducer1,
+      reducer2
+    });
+
+    const initialState = {
+      reducer1: 0,
+      reducer2: 2
+    };
+    const finalState = combinedReducer(initialState, {}, 1);
+    expect(finalState).toEqual({
+      reducer1: 1,
+      reducer2: {
+        reducer3: {
+          extState: 1,
+          subExtState: 2
+        }
+      }
+    });
   });
 
 });
